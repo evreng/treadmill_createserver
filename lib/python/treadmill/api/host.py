6d1a97b522a579029f4df8f1333493652fb9e47e
Evren La Torre <evreng@users.noreply.github.com>
2018-03-22 18:00:46 +0000

    API: added basic host API functionalities

--- lib/python/treadmill/api/host.py
+++ lib/python/treadmill/api/host.py
@@ -1,17 +1,33 @@
-from treadmill import authz
-
+# from treadmill import authz
+from treadmill.aws.manager import HostManager
 
 class API(object):
     """Treadmill Host REST API."""
 
-    def __init__(self):
+    def __init__(self, ):
+        host_manager = HostManager()
 
-        def create_server():
-            return ''
+        def list_servers(hostname=""):
+            return host_manager.getHost(hostname)
 
-        def delete_server():
-            return ''
+        def create_server(name, domain, ami, subnet, proxy, secgroup, size="t2.micro", key="tm-internal", role="Node", count=1):
+            host_manager.createHost(
+                {'image_id': ami,
+                 'count': count,
+                 'instance_type': size,
+                 'secgroup_ids': secgroup,
+                 'fqdn': '{}.{}'.format(name, domain),
+                 'cell': subnet,
+                 'key': key,
+                 'role': role,
+                 'proxy': proxy
+                 }
+            )
 
+        def delete_server(hostname):
+            host_manager.deleteHost(hostname)
+
+        self.list_servers = list_servers
         self.create_server = create_server
         self.delete_server = delete_server
 
